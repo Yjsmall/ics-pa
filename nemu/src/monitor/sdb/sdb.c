d2a79da9d80bcdeadf24d385487af02a09817722
tracer-ics2024 <tracer@njuics.org>
2024-09-10 20:07:54 +0800

    >  compile NEMU todo Fisher Linux fisher 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  20:07:54 up  3:57,  2 users,  load average: 0.02, 0.05, 0.04

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -87,9 +87,9 @@ static int cmd_x(char *args) {
   int    result = sscanf(args, "%d %x", &bytes, &address);
   if (result == 2) {
     for (word_t offset = 0; offset < bytes; ++offset) {
-      printf(FMT_WORD " ", (address + offset * 4));
+      //printf(FMT_WORD " ", (address + offset * 4));
       for (word_t byte = 0; byte < 4; ++byte) {
-        printf("addr " FMT_WORD " ", (address + byte));
+        printf("addr " FMT_WORD " \n", (address + byte));
         uint8_t *value = guest_to_host(address + byte);
         printf("%x ", *value);
       }
