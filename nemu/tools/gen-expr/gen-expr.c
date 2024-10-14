d40f9d221b23a830d3288476f3906f618385b47d
tracer-ics2024 <tracer@njuics.org>
2024-10-14 21:09:42 +0800

    >  compile NEMU todo Fisher Linux fisher 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  21:09:42 up  5:55,  2 users,  load average: 0.12, 0.12, 0.09

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -31,64 +31,8 @@ static char *code_format =
     "  return 0; "
     "}";
 
-static char *buf_start = NULL;
-static char *buf_end   = buf + (sizeof(buf) / sizeof(buf[0]));
-
-static int choose(int n) {
-  return rand() % n;
-}
-
-static void gen_space() {
-  int size = choose(4);
-  if (buf_start < buf_end) {
-    int n_writes = snprintf(buf_start, buf_end - buf_start, "%*s", size, "");
-    if (n_writes > 0) {
-      buf_start += n_writes;
-    }
-  }
-}
-
-static void gen_num() {
-  int num = choose(INT8_MAX);
-  if (buf_start < buf_end) {
-    int n_writes = snprintf(buf_start, buf_end - buf_start, "%d", num);
-    if (n_writes > 0) {
-      buf_start += n_writes;
-    }
-  }
-  gen_space();
-}
-
-static void gen_char(char c) {
-  int n_writes = snprintf(buf_start, buf_end - buf_start, "%c", c);
-  if (buf_start < buf_end) {
-    if (n_writes > 0) {
-      buf_start += n_writes;
-    }
-  }
-}
-
-static char ops[] = {'+', '-', '*', '/'};
-static void gen_rand_op() {
-  int  op_index = choose(sizeof(ops));
-  char op       = ops[op_index];
-  gen_char(op);
-}
-
 static void gen_rand_expr() {
-  switch (choose(3)) {
-    case 0: gen_num(); break;
-    case 1:
-      gen_char('(');
-      gen_rand_expr();
-      gen_char(')');
-      break;
-    default:
-      gen_rand_expr();
-      gen_rand_op();
-      gen_rand_expr();
-      break;
-  }
+  buf[0] = '\0';
 }
 
 int main(int argc, char *argv[]) {
@@ -100,7 +44,6 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i++) {
-    buf_start = buf;
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
@@ -110,14 +53,14 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc -Werror=div-by-zero /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
     int result;
-    ret = fscanf(fp, "%d", &result);
+    fscanf(fp, "%d", &result);
     pclose(fp);
 
     printf("%u %s\n", result, buf);
